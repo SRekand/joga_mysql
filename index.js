54a2b4027e68988781beb59e9f9a8c077f728da8
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-02-08 15:44:51 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -47,6 +47,18 @@ app.get('/', (req, res) => {
     })
 });
 
+//show article by this slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result
+        res.render('article', {
+            article: article
+        })
+    });
+});
 
 // app start point
 app.listen(3000, () => {
